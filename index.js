36971c10337a97605e61176bf5a92e9174927c3c
marcus featherston <marcusovfeatherston@gmail.com>
2023-02-06 10:41:53 -0700

    function for using find method to iterate for W years

--- index.js
+++ index.js
@@ -1,6 +1,71 @@
 const superbowlWin = (listOfRecords) => {
   const foundRecord = listOfRecords.find((record) => {
+    // console.log(record);
     return record.result === "W";
   });
-  return foundRecord ? foundRecord.year : foundRecord;
+  console.log(foundRecord);
+  return foundRecord ? foundRecord.year : undefined;
 };
+// //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// const superbowlWin = (listOfRecords) => {
+//   const foundRecord = listOfRecords.find(({ result }) => {
+//     return result === "W";
+//   });
+//   return foundRecord ? foundRecord.year : foundRecord;
+// };
